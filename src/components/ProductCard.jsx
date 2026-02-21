3b52307c8b7f857f77e32f7e5870e0a7d59ece5b
akida-design <m.akida.info@gmail.com>
2026-02-21 22:32:10 +0300

    updated files

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,21 +1,19 @@
 import styles from "../styles/ProductCard.module.css"
-import { Card, CardContent, Typography, Chip } from "@mui/material"
+import { Card, CardContent, Typography, Chip, Box } from "@mui/material"
 
 export default function ProductCard({ product }) {
     const { name, price, inStock } = product;
 
     return (
-        <>
-            <Card className={inStock ? styles.card : `${styles.card} ${styles.outOfStock}`}>
-                <CardContent>
-                    <Typography variant="h6" component="div" gutterBottom>{name}</Typography>
-                    <Typography variant="body2" color="textSecondary">Price: KES {price.toFixed(2)}</Typography>
+        <Card className={inStock ? styles.card : `${styles.card} ${styles.outOfStock}`}>
+            <CardContent className={styles.cardContent}>
+                <Typography className={styles.name} variant="h6" component="div" gutterBottom>{name}</Typography>
+                <Typography className={styles.price} variant="body2">Price: KES {price.toFixed(2)}</Typography>
 
-                    <div className={styles.row}>
-                        <Chip label={inStock ? "In stock" : "Out of stock"} color={inStock ? "success" : "default"} size="small" />
-                    </div>
-                </CardContent>
-            </Card>
-        </>
+                <Box className={styles.row}>
+                    <Chip label={inStock ? "In stock" : "Out of stock"} color={inStock ? "success" : "default"} size="small" />
+                </Box>
+            </CardContent>
+        </Card>
     );
 }
